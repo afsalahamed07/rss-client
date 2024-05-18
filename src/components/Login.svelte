780c66894af4b19820143942b3ce206b804b5e54
afsalahamed07 <afsalahamed07@gmail.com>
2024-05-18 16:26:55 +0530

    Cleaning Login page

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -21,23 +21,45 @@
     }
 </script>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card mt-5">
-                <div class="card-header">
-                    <h4>Login</h4>
+<div class="container vh-100 d-flex align-items-center justify-content-center">
+    <div class="row justify-content-center w-100">
+        <div class="col-md-8 col-lg-8 border"> <!-- Adjust the width here -->
+            <div class="card p-5 pt-5 m-5 border-0">
+                <div data-mdb-input-init class="input-group mb-5">
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                    <input type="text" class="form-control" id="username" placeholder="Username" bind:value={username}/>
                 </div>
-                <div class="card-body">
-                    <div class="mb-3">
-                        <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="username" bind:value={username}>
+
+                <!-- Password input -->
+                <div data-mdb-input-init class="input-group mb-5">
+                    <input type="password" id="password" class="form-control" placeholder="Password"
+                           bind:value={password}/>
+                </div>
+
+                <!-- 2 column grid layout for inline styling -->
+                <div class="row mb-5">
+                    <div class="col d-flex justify-content-center">
+                        <!-- Checkbox -->
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked/>
+                            <label class="form-check-label" for="form2Example31"> Remember me </label>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" bind:value={password}>
+
+                    <div class="col">
+                        <!-- Simple link -->
+                        <a class="link-underline link-underline-opacity-0" href="#!">Forgot password?</a>
                     </div>
-                    <button class="btn btn-primary" on:click={login}>Login</button>
+                </div>
+
+
+                <button type="button" data-mdb-button-init data-mdb-ripple-init
+                        class="btn btn-primary btn-block mb-4" on:click={login}>Sign in
+                </button>
+
+                <div class="text-center">
+                    <p>Not a member? <a class="link-underline link-underline-opacity-0"
+                                        href="/register">Register</a></p>
                 </div>
             </div>
         </div>
